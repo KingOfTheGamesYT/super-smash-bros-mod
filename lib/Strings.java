0b6f4b3189d7703b3752fc92213c5288937d3525
KingOfTheGamesYT <53309562+KingOfTheGamesYT@users.noreply.github.com>
2022-10-25 18:30:55 -0400

    removed
    they're in the main class

--- lib/Strings.java
+++
@@ -1,9 +0,0 @@
-package com.jawser.lib;
-
-public class Strings {
-	
-	public static final String MODID = "jawser";
-	public static final String name = "Super Smash Bros";
-	public static final String version = "1.0";
-
-}
